4291368716c1bf64a07d963de8dbd944f805191a
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-06-12 19:11:33 -0300

    Guardar Última etapa de registro

--- src/screens/Auth/RegisterSteps/RegisterCuatro.jsx
+++ src/screens/Auth/RegisterSteps/RegisterCuatro.jsx
@@ -1,104 +1,201 @@
-import React, { useState, useEffect } from "react";
-import { Image, StyleSheet, View, Text, TouchableOpacity, TextInput, FlatList } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Picker } from "@react-native-picker/picker";
+import { CheckBox } from "react-native-elements";
 
 function RegisterCuatro() {
+  const [city, setCity] = useState("");
+  const [isSpanish, setIsSpanish] = useState(false);
+  const [madridZone, setMadridZone]=useState('');
   return (
     <View style={styles.container}>
-                <Image
-          source={require("../../../assets/Login/Ellipse 1.png")}
-          resizeMode="contain"
-          style={styles.eclipse1}
-        />
-                       <Image
-          source={require("../../../assets/Login/Ellipse 1.png")}
-          resizeMode="contain"
-          style={styles.eclipse2}
-        />
-             <TouchableOpacity style={styles.closeButton} onPress={() => alert('Salir')}>
+      <Image
+        source={require("../../../assets/Login/smallIcon.png")}
+        resizeMode="contain"
+        style={styles.logoApp}
+      />
+      <Image
+        source={require("../../../assets/Login/Ellipse 1.png")}
+        resizeMode="contain"
+        style={styles.eclipse1}
+      />
+      <Image
+        source={require("../../../assets/Login/Ellipse 1.png")}
+        resizeMode="contain"
+        style={styles.eclipse2}
+      />
+      <TouchableOpacity
+        style={styles.closeButton}
+        onPress={() => alert("Salir")}
+      >
         <Ionicons name="arrow-back" size={20} color="gray" />
-      </TouchableOpacity>    
+      </TouchableOpacity>
       <View style={styles.titles}>
         <Text style={styles.h1}>Última etapa de registro</Text>
         <Text style={styles.h6}>
-            ¡Ya falta poco para ser parte de la comunidad de Mágic!
+          ¡Ya falta poco para ser parte de la comunidad de Mágic!
         </Text>
       </View>
-    <View style={styles.inputsConainer}>
-    <TextInput style={styles.input} placeholder="País de Nacimiento" />
-  <TextInput 
-    style={styles.input} 
-    placeholder="Teléfono de contacto" 
-    keyboardType="numeric" // Para aceptar solo números
-  />
-  <TextInput style={styles.input} placeholder="Cargo o profesión" />
-  <TextInput style={styles.input} placeholder="Película favorita" />
-  <TextInput style={styles.input} placeholder="Deportes favoritos" />
-  <TextInput 
-    style={[styles.input, { height: 100 }]} // Hacer el campo más grande para "Hobbies"
-    placeholder="Hobbies"
-    multiline // Permitir varias líneas de texto
-  />
+      <ScrollView style={styles.inputsConainer}>
+        <TextInput style={styles.input} placeholder="País de Nacimiento" />
+        <TextInput
+          style={styles.input}
+          placeholder="Teléfono de contacto"
+          keyboardType="numeric"
+        />
+        <TextInput style={styles.input} placeholder="Cargo o profesión" />
+        <TextInput style={styles.input} placeholder="Película favorita" />
+        <TextInput style={styles.input} placeholder="Deportes favoritos" />
+        <TextInput style={styles.input} placeholder="Hobbies" />
+        <Text style={styles.h6bis}>Información geográfica</Text>
+        <Text style={styles.h6}>Ciudad Actual</Text> 
+        <Picker
+          selectedValue={city}
+          style={styles.pickerCity}
+          onValueChange={(itemValue, itemIndex) => {
+            if (itemValue !== "") {
+              setCity(itemValue);
+            }
+          }}
+        >
+          <Picker.Item label="Ciudad Actual" value="" />
+          <Picker.Item label="Madrid" value="Madrid" />
+        </Picker>
+        <CheckBox
+          title="He nacido en España (No obligatorio)."
+          checked={isSpanish}
+          onPress={() => setIsSpanish(!isSpanish)}
+          containerStyle={styles.checkboxContainer}
+          textStyle={styles.checkboxText}
+          style={{color:'gray'}}
+        />
+        {isSpanish &&( <>
+         <Text style={styles.h6}>Comunidad Autónoma Natal</Text> 
+        <Picker
+          selectedValue={city}
+          style={styles.pickerCity}
+          onValueChange={(itemValue, itemIndex) => {
+              setCity(itemValue);
+          }}
+        >
+          <Picker.Item label="Comunidad Autónoma Natal" value="" />
+          <Picker.Item label="Madrid" value="Madrid" />
+        </Picker></>)}
+        <Picker
+          selectedValue={madridZone}
+          style={styles.pickerCity}
+          onValueChange={(itemValue, itemIndex) => setMadridZone(itemValue)}
+        >
+          <Picker.Item label="Zona de Madrid" value="" />
+          <Picker.Item label="Norte" value="Norte" />
+          <Picker.Item label="Sur" value="Sur" />
+          <Picker.Item label='Centro' value="Centro"/>
+          <Picker.Item label='Este' value="Este"/>
+          <Picker.Item label='Oeste' value="Oeste"/>
+        </Picker>
+
+      </ScrollView>
     </View>
-        <Text></Text></View>
-  )
+  );
 }
 
-export default RegisterCuatro
+export default RegisterCuatro;
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width: "100%",
-      height: '100vh',
-      alignItems: 'center',
-    },
-    eclipse1: {
-        position: "absolute",
-        top: -99,
-        right: -30,
-      },
-      eclipse2: {
-        position: "absolute",
-        top: -60,
-        left: 150,
-      },  titles: {
-        paddingTop: 35,
-        alignItems: 'center',
-      },
-      h1: {
-        fontSize: 19,
-        marginVertical: 10,
-        marginTop: 30,
-        color: 'gray',
-      },
-      h6: {
-        color: 'gray',
-        fontSize: 12,
-        textAlign: 'center',
-        marginHorizontal: 20,
-      },
-      closeButton: {
-        position: 'absolute',
-        top: 40,
-        left: 5,
-        width: 30,
-        height: 30,
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: 1,
-      },
-      inputsConainer: {
-        width:'80%',
-},
-input: {
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+  },
+  eclipse1: {
+    position: "absolute",
+    top: -99,
+    right: -30,
+  },
+  eclipse2: {
+    position: "absolute",
+    top: -60,
+    left: 150,
+  },
+  titles: {
+    paddingTop: 35,
+    alignItems: "center",
+  },
+  h1: {
+    fontSize: 19,
+    marginVertical: 10,
+    marginTop: 30,
+    color: "gray",
+  },
+  h6: {
+    color: "gray",
+    fontSize: 12,
+    textAlign: "center",
+    marginHorizontal: 20,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 40,
+    left: 5,
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+  inputsConainer: {
+    width: "80%",
+    marginTop: 20,
+  },
+  input: {
     borderBottomWidth: 1,
-    borderBottomColor: '#8F8F8F', // Gris claro
+    borderBottomColor: "#8F8F8F", // Gris claro
     padding: 7,
     marginBottom: 10,
-    width: '100%',
+    width: "100%",
   },
-  inputTextColor: {
-    color:'gray'
-  }})
+  logoApp: {
+    width: 50,
+    height: 50,
+    marginHorizontal: "auto",
+    marginTop: 50,
+    zIndex: 99999,
+    marginBottom: -20,
+  },
+  h6bis: {
+    color: "gray",
+    marginTop: 30,
+    marginHorizontal: "auto",
+    fontSize: 12,
+  },
+  pickerCity: {
+    color: "gray",
+    marginTop: 10,
+  },
+  checkboxContainer: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    marginTop: 15,
+  },
+  checkboxText: {
+    fontWeight: "normal",
+    fontSize: 10,
+  },
+  h6: {
+    fontSize: 10,
+    fontWeight: "normal",
+    color:'gray',
+    marginTop:20,
+    marginBottom:-15
+  }
+});
