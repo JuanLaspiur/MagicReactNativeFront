056d9ab1873b5a1040404c81a521ab2dd67a3505
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-06-12 10:47:19 -0300

    En téoria tendría que estar bien el Login

---
+++ src/screens/Auth/Register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+const Register = () => {
+    return(<View></View>)
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
