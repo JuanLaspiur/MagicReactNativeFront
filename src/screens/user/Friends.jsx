6c7cb2620dcfe60c345efe2e7b221739b296008d
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-06-18 12:21:59 -0300

    Friends container

--- src/screens/user/Friends.jsx
+++ src/screens/user/Friends.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TextInput, ScrollView, Image } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons'; // Importa FontAwesome desde @expo/vector-icons
 import AppHeader from '../../components/User/AppHeader';
 import ItemsFriends from '../../components/User/Friends/ItemsFriends';
@@ -22,11 +22,49 @@ function Friends() {
         <ItemsFriends name="Carlos" lastName="López" age={30} avatarUrl={'https://this-person-does-not-exist.com/img/avatar-gen1107514e9bf4f1898b0f129c5b55616f.jpg'} />
         {/* Agrega más ItemsFriends según sea necesario */}
       </ScrollView>
+      <Image
+        source={require("../../assets/Login/Ellipse 1.png")}
+        style={styles.eclipse1}
+          resizeMode="contain"
+      />
+          <Image
+        source={require("../../assets/Login/Ellipse 1.png")}
+        style={styles.eclipse2}
+          resizeMode="contain"
+      />
+            <Image
+        source={require("../../assets/Login/Ellipse 1.png")}
+        resizeMode="contain"
+        style={styles.eclipse3}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  eclipse1: {
+    position: "absolute",
+    top: -10,
+    right: -105,
+    zIndex:-1,
+    opacity:0.2
+  }, 
+   eclipse2: {
+    position: "absolute",
+    height: 500,
+    top: 90,
+    left: -105,
+    zIndex:-1,
+    opacity:0.2
+  },
+  eclipse3: {
+    position: "absolute",
+    height: 300,
+    top: 50,
+    left: 0,
+    zIndex:-1,
+    opacity:0.2
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
