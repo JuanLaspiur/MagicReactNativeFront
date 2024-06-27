b1ca9d2961a54e6cb9acb84c33e5b0933fd61a02
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-06-27 16:48:36 -0300

    Agrege los animales a los contactos

--- src/components/User/Friends/ItemsFriends.jsx
+++ src/components/User/Friends/ItemsFriends.jsx
@@ -5,30 +5,112 @@ import { Ionicons } from '@expo/vector-icons';
 import { getUserById } from '../../../api/User.controller'
 import env from '../../../../env.js'
 
+import { getAnimales } from "../../../api/User.controller.js";
+import { obtenerNumerosDespuesGuion } from "../../../helpers/animalGetOnlyNumber.js";
+
+
 function ItemsFriends({ name, lastName, age, avatarUrl, userID }) {
   const [user, setUser] = useState([]);
   const [imageUri, setImageUri] = useState('');
+  const navigation = useNavigation();
+
+  const [animalsList, setAnimalsList] = useState([]);
+  const [animal, setAnimal] = useState(null);
+  const [numero, setNumero] = useState(obtenerNumerosDespuesGuion("../../../assets/Animals/ICONOS A COLOR-00.png"))
+  
+  const animalImages = [
+    require("../../../assets/Animals/ICONOS A COLOR-00.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-01.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-02.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-03.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-04.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-05.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-06.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-07.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-08.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-09.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-10.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-11.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-12.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-13.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-14.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-15.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-16.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-17.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-18.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-19.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-20.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-21.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-22.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-23.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-24.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-25.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-26.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-27.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-28.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-29.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-30.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-31.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-32.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-33.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-34.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-35.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-36.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-37.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-38.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-39.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-40.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-41.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-42.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-43.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-44.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-45.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-46.png"),
+    require("../../../assets/Animals/ICONOS A COLOR-47.png"),
+  ];
+
+  
 
  const obtenerUsuario = async() => {
   const response  = await getUserById(userID)
   setUser(response.data)
-}
-
-  useEffect(()=>{
-    setImageUri(env.BACK_URL + '/perfil_img/' + user._id);
-    obtenerUsuario()
-  },[])
-
-
-  const navigation = useNavigation();
-
-  const handleAvatarPress = () => {
+}  
+const handleAvatarPress = () => {
     navigation.navigate('OtherUserProfile')
   };
 
   const handleMessagePress = () => {
     Alert.alert('Llendo al chat..');
   };
+
+
+  useEffect(()=>{
+    setImageUri(env.BACK_URL + '/perfil_img/' + user._id);
+    obtenerUsuario()
+    const getAllAnimals = async () => {
+      try {
+        const data = await getAnimales();
+        setAnimalsList(data);
+      } catch (error) {
+        console.log("Error al obtener la lista de animales " + error);
+      }
+    };
+    getAllAnimals();
+  },[])
+
+  useEffect(() => {
+    if (animalsList.length > 0 && user.animal) {
+      let myAnimalInTheList = animalsList.find((item) => item._id === user.animal);
+      setAnimal(myAnimalInTheList);
+      if (myAnimalInTheList) {
+        setNumero(obtenerNumerosDespuesGuion(myAnimalInTheList.img)); 
+      } else if (user.animal_img) {
+        setNumero(obtenerNumerosDespuesGuion(user.animal_img));
+      }
+    }
+  }, [animalsList, user]);
+
+
 
   return (
     <TouchableOpacity style={styles.card} onPress={handleAvatarPress}>
@@ -60,10 +142,10 @@ function ItemsFriends({ name, lastName, age, avatarUrl, userID }) {
         )}
         <View style={styles.textContainer}>
           <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.description}>{`${user.last_name} - ${user.age} años`}</Text>
+          <Text style={styles.description}>{`${user.last_name === null ? '           ' : user.last_name} - ${user.age} años`}</Text>
         </View>
         <View style={styles.avatarAnimalContainer}>
-          <Image source={require('../../../assets/Animals/ICONOS A COLOR-13.png')} style={styles.avatarAnimal} />
+          <Image     source={animalImages[numero]} style={styles.avatarAnimal} />
         </View>
       </View>
       <TouchableOpacity onPress={handleMessagePress} style={styles.messageContainer}>
