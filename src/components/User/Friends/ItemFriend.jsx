5b811b6bf186612695cec13eac8b54aa16ad518f
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-06-28 18:04:04 -0300

    Carga bien

--- src/components/User/Friends/ItemFriend.jsx
+++ src/components/User/Friends/ItemFriend.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import env from "../../../../env";
-
+import { FontAwesome } from "@expo/vector-icons";
 
 function ItemFriend({ name, lastName, user }) {
   const imageUri =  env.BACK_URL + '/perfil_img/' + user._id;
@@ -12,84 +12,74 @@ function ItemFriend({ name, lastName, user }) {
     navigation.navigate('OtherUserProfile', { user });
   };
 
-
   return (
-    <TouchableOpacity style={styles.avatarContainer} onPress={handlePressItemFriend}>
-     <View style={styles.avatarContainer}>
-     {imageUri ? (
+    <TouchableOpacity style={styles.card} onPress={handlePressItemFriend}>
+      <View style={styles.avatarContainer}>
+        {imageUri ? (
           <Image source={{ uri: imageUri }}  style={styles.avatar} />
         ) : (
           <FontAwesome name="user-circle-o" size={50} color="#AED0F6" />
         )}
-          <View style={styles.textContainer}>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.description}> {lastName}</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.description}>{lastName}</Text>
         </View>
+        {/* Componente de Mensaje (Ejemplo) */}
+        <TouchableOpacity style={styles.messageContainer}>
+          <FontAwesome name="comment-o" size={24} color="#AED0F6" />
+        </TouchableOpacity>
       </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        width: "100%",
-        backgroundColor: "#F2F2F2",
-        borderRadius: 10,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 4,
-        elevation: 2,
-        padding: 15,
-        marginVertical: 5,
-        alignSelf: "center",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-      },
-      avatarContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap:10
-      },
-      avatar: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        marginRight: 15,
-      },
-      name: {
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "#333333",
-        marginBottom: 5,
-      },
-      description: {
-        fontSize: 14,
-        color: "#666666",
-      },
-      messageContainer: {
-        width: 35,
-        height: 35,
-        borderRadius: 20,
-        borderWidth: 1,
-        borderColor: "#AED0F6",
-        justifyContent: "center",
-        alignItems: "center",
-      },
-      avatarAnimalContainer: {
-        width: 25,
-        height: 25,
-        borderRadius: 25,
-        overflow: 'hidden',
-        opacity: 0.5,
-        marginTop:19
-      },
-      avatarAnimal: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
-      },
+  card: {
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3,
+    padding: 15,
+    marginVertical: 8,
+    alignSelf: "center",
+  },
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    marginRight: 15,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333333",
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 14,
+    color: "#666666",
+  },
+  messageContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#AED0F6",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default ItemFriend;
--- src/screens/user/Friends.jsx
+++ src/screens/user/Friends.jsx
@@ -10,17 +10,16 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import AppHeader from "../../components/User/AppHeader";
 import { todosLosContactos } from "../../api/User.controller.js";
-import ItemFriend from "../../components/User/Friends/ItemFriend.jsx"; // Asegúrate de importar correctamente ItemFriend
+import ItemFriend from "../../components/User/Friends/ItemFriend.jsx";
 
 function Friends() {
   const [allList, setAllList] = useState([]);
+  const [itemsToShow, setItemsToShow] = useState(10);
 
   useEffect(() => {
     const fetchAllUsers = async () => {
       try {
         const response = await todosLosContactos();
-        console.log('Lista de usuarios:', response); 
-  
         setAllList(response);
       } catch (error) {
         console.error('Error al obtener todos los usuarios:', error);
@@ -51,6 +50,15 @@ function Friends() {
     }
   };
 
+  const handleScroll = (event) => {
+    const { layoutMeasurement, contentOffset, contentSize } = event.nativeEvent;
+    const paddingToBottom = 20;
+    if (layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom) {
+      // Cuando llega al final del ScrollView, cargar más elementos
+      setItemsToShow(itemsToShow + 10); // Cargar 10 elementos más
+    }
+  };
+
   return (
     <View style={styles.container}>
       <AppHeader title="Amigos" />
@@ -75,8 +83,12 @@ function Friends() {
           />
         </TouchableOpacity>
       </View>
-      <ScrollView style={styles.scrollContainer}>
-        {allList && allList.length > 0 &&  allList.map((user) => (
+      <ScrollView
+        style={styles.scrollContainer}
+        onScroll={handleScroll}
+        scrollEventThrottle={16} 
+      >
+        {allList.slice(0, itemsToShow).map((user) => (
           <ItemFriend
             key={user._id}
             name={user.name}
@@ -88,6 +100,7 @@ function Friends() {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   eclipse1: {
