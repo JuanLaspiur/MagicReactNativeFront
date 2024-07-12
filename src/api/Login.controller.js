a37854190ebee36879d0b765d22e1fc577751dd3
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-07-12 11:09:40 -0300

    guardar

--- src/api/Login.controller.js
+++ src/api/Login.controller.js
@@ -5,16 +5,17 @@ import axios from 'axios';
 import env from '../../env';
 
 const login = async (email, password) => {
-  try {
+ try {
     const response = await api.post('/loginReact', { email, password });
+    await saveToSecureStore("token", response.data.SESSION_INFO.token);
+   console.log(response)
 
-    await saveToSecureStore("token", response.data.token);
+   setTokenString(response.data.SESSION_INFO.token);
+   
 
-    setTokenString(response.data.token);
-
-    const userResponse = await axios.get(`${env.BACK_URL}/user_by_id/${response.data.user_id}`, {
+    const userResponse = await axios.get(`${env.BACK_URL}/user_by_id/${response.data.SESSION_INFO._id}`, {
       headers: {
-        Authorization: `Bearer ${response.data.token}`
+        Authorization: `Bearer ${response.data.SESSION_INFO.token}`
       }
     });
 
@@ -24,15 +25,19 @@ const login = async (email, password) => {
     } else {
 
     await saveToSecureStore("user", JSON.stringify(userResponse.data));
+    console.log('User del LoginController.js : ', JSON.stringify(userResponse.data))
 
-    return response.data.token; }
+    return response.data.SESSION_INFO.token; 
+    
+    }
   } catch (error) {
-    console.error('Error al iniciar sesión:', error);
-    alert('Error al iniciar sesión. Revise su contraseña o su correo.');
-    throw error; 
+   console.error('Error al iniciar sesión:', error);
+   alert('Error al iniciar sesión. Revise su contraseña o su correo.');
+   throw error; 
   }
 };
 
 export { login };
+
 
 
