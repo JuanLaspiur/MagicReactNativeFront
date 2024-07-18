728d73ae5bc7c6b608405b67b56007a41b3a1ba5
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-07-18 16:43:45 -0300

    Guardar

--- src/components/User/Modals/NotificationsModal.jsx
+++ src/components/User/Modals/NotificationsModal.jsx
@@ -1,105 +1,144 @@
 import React, { useEffect, useState } from "react";
-import { Modal, View, Text, StyleSheet, TouchableOpacity, FlatList } from "react-native";
-import { getValueFromSecureStore } from '../../../helpers/ExpoSecureStore';
-import { getNotificationsWebApi, updateAllNotificationsWebApiSeen } from "../../../api/Notifications.controller";
+import {
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { getValueFromSecureStore } from "../../../helpers/ExpoSecureStore";
+import {
+  getNotificationsWebApi,
+  updateAllNotificationsWebApiSeen,
+} from "../../../api/Notifications.controller";
+import env from "../../../../env.js";
 
 const NotificationsModal = ({ visible, onClose }) => {
-    const [authUser, setAuthUser] = useState(null);
-    const [notifications, setNotifications] = useState([]);
-    const [flag, setFlag] = useState(false)
+  const [authUser, setAuthUser] = useState(null);
+  const [notifications, setNotifications] = useState([]);
+  const [flag, setFlag] = useState(false);
 
+  useEffect(() => {
+    const getAuthUser = async () => {
+      const response = await getValueFromSecureStore("user");
+      setAuthUser(JSON.parse(response));
+    };
 
-    useEffect(() => {
-        const getAuthUser = async () => {
-            const response = await getValueFromSecureStore("user");
-            setAuthUser(JSON.parse(response));
-        };
+    const getAllWebNotification = async () => {
+      const response = await getNotificationsWebApi();
+      setNotifications(response);
+      console.log("Notificaciones " + JSON.stringify(response));
+    };
 
-        const getAllWebNotification = async () => {
-            const response = await getNotificationsWebApi();
-            setNotifications(response);
-            console.log('Notificaciones '+JSON.stringify(response))
-        };
+    getAuthUser();
+    getAllWebNotification();
+  }, [flag]);
 
-        getAuthUser();
-        getAllWebNotification();
-    }, [flag]);
-
-    const renderNotification = ({ item }) => (
-        <View style={[
-            styles.notification,
-            { backgroundColor: item.visto ? 'white' : '#E0F0FF' } // Color más claro similar a #AED0F6
-        ]}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.message}>{item.message}</Text>
-            <Text style={styles.date}>{item.date}</Text>
-        </View>
-    );
-    
+  const renderNotification = ({ item }) => {
+    const imageUrl = item.perfil
+      ? `${env.BACK_URL}/perfil_img/${item.perfil}`
+      : `${env.BACK_URL}/quedada_img/${item.quedada}`;
 
     return (
-        <Modal visible={visible} transparent={true} animationType="slide">
-            <View style={styles.modalContainer}>
-                <View style={styles.modalContent}>
-                    <TouchableOpacity onPress={async()=>{
-                
-                       await updateAllNotificationsWebApiSeen()
-                       setFlag(!flag)   
-                       onClose()
-                        }} style={styles.closeButton}>
-                        <Text style={styles.closeButtonText}>Cerrar</Text>
-                    </TouchableOpacity>
-                    <FlatList
-                        data={notifications}
-                        renderItem={renderNotification}
-                        keyExtractor={(item) => item._id}
-                    />
-                </View>
-            </View>
-        </Modal>
+      <View
+        style={[
+          styles.notification,
+          { backgroundColor: item.visto ? "white" : "#E0F0FF" },
+        ]}
+      >
+        <View style={styles.textContainer}>
+          <Image style={styles.icon} source={{ uri: imageUrl }} />
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.message}>{item.message}</Text>
+          <Text style={styles.date}>{item.date}</Text>
+        </View>
+      </View>
     );
+  };
+
+  return (
+    <Modal visible={visible} transparent={true} animationType="slide">
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <TouchableOpacity
+            onPress={async () => {
+              await updateAllNotificationsWebApiSeen();
+              setFlag(!flag);
+              onClose();
+            }}
+            style={styles.closeButton}
+          >
+            <Text style={styles.closeButtonText}>Cerrar</Text>
+          </TouchableOpacity>
+          <FlatList
+            data={notifications}
+            renderItem={renderNotification}
+            keyExtractor={(item) => item._id}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
 };
 
+
 const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-    },
-    modalContent: {
-        width: "98%",
-        backgroundColor: "white",
-        borderRadius: 10,
-        padding: 20,
-        alignItems: "center",
-    },
-    closeButton: {
-        alignSelf: "flex-end",
-        padding: 10,
-    },
-    closeButtonText: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "#007BFF",
-    },
-    notification: {
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: "#ddd",
-    },
-    title: {
-        fontSize: 16,
-        fontWeight: "bold",
-    },
-    message: {
-        fontSize: 14,
-        color: "#555",
-    },
-    date: {
-        fontSize: 12,
-        color: "#999",
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bellIconContainer: {
+    position: 'absolute',
+    top: 50,
+    right: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '98%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+  },
+  closeButton: {
+    alignSelf: 'flex-end',
+  },
+  closeButtonText: {
+    color: 'blue',
+    fontSize: 16,
+  },
+  notification: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  textContainer: {
+    marginLeft: 10,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  message: {
+    color: 'gray',
+  },
+  date: {
+    color: 'gray',
+    fontSize: 12,
+  },
 });
 
 export default NotificationsModal;
